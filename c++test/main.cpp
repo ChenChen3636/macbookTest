53957750ff7a9cae59fa5d0a9a309ab02fb21baa
Pei Chen Chung <sunsun4223@gmail.com>
2022-08-31 23:21:26 +0800

    Initial Commit

---
+++ c++test/main.cpp
@@ -0,0 +1,13 @@
+//
+//  main.cpp
+//  c++test
+//
+//  Created by 鍾佩宸 on 2022/8/31.
+//
+
+#include <iostream>
+using namespace std;
+int main(int argc, const char * argv[]) {
+    cout << "Hi, I'm Ellie." << endl;
+    return 0;
+}
